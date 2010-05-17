673c45675b4592b27aaebbfe4069ccf748f5e169
doug tangren <d.tangren@gmail.com>
2010-05-16 22:12:02 -0400

    guard clauses and dialog for selecting trip

--- src/com/njtransit/Main.java
+++ src/com/njtransit/Main.java
@@ -3,7 +3,9 @@ package com.njtransit;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,15 +21,20 @@ import com.njtransit.domain.StopTime;
 import com.njtransit.domain.Trip;
 
 public class Main extends Activity implements LocationListener {
-    /** Called when the activity is first created. */
-	
+    
 	private NJTransitDBAdapter adapter;   
 	
 	private LocationManager locationManager;
 	
 	private Session session = Session.get();
 	
-    @Override
+	private void warn(CharSequence msg) {
+		//Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+		Log.e("err!", msg.toString());
+	}
+	
+	/** Called when the activity is first created. */
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
@@ -45,25 +52,54 @@ public class Main extends Activity implements LocationListener {
 
 			@Override
 			protected Integer doInBackground(Void... params) {
-				// TODO Auto-generated method stub
 				Long now = SystemClock.currentThreadTimeMillis();
 				adapter = new NJTransitDBAdapter(Main.this).open();
+				
 				Long later = SystemClock.currentThreadTimeMillis();
 				Log.d("database", later - now + " seconds");
+				
 				ArrayList<Station> stations = adapter.getAllStations();
-				session.setStations(stations);
-				ArrayList<Trip> trips = adapter.getTrips(session.findClosestStation(null));
-				for(Trip t : trips) {
-					Log.i("trip", t.getHeadsign());
+				if(stations != null) {
+					warn("could not retrieve stations");
+				} else {
+					session.setStations(stations);
 				}
-				ArrayList<Station> stations2 = session.findClosestStations(null, 6);
-				for(Station s : stations2) {
-					Log.i("station", s.getName());
-					ArrayList<Trip> tripz = adapter.getTrips(s);
-					for(Trip t : tripz) {
-						ArrayList<StopTime> stopTimes = adapter.getAllStopTimes(s, t);
-						for(StopTime st : stopTimes) {
-							
+				
+				ArrayList<Trip> trips = adapter.getTrips(session.findClosestStation(null));
+				if(trips == null) {
+					warn("could not retrieve trips");
+				} else {
+					for(Trip t : trips) {
+						Log.i("trip", t.getHeadsign());
+					}
+					
+					final CharSequence[] tripNames = new CharSequence[trips.size()];
+					for(int i=0;i<trips.size();i++) {
+						tripNames[i] = trips.get(i).getHeadsign();
+					}
+					
+					AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
+					builder.setTitle("Select a trip");
+					builder.setItems(tripNames, new DialogInterface.OnClickListener() {
+					    public void onClick(DialogInterface dialog, int item) {
+					        warn(tripNames[item]);
+					    }
+					});
+					AlertDialog alert = builder.create();
+				}
+				
+				ArrayList<Station> closestStations = session.findClosestStations(null, 6);
+				if(closestStations == null) {
+					warn("could not retrieve closest stations");
+				} else {
+					for(Station s : closestStations) {
+						Log.i("station", s.getName());
+						ArrayList<Trip> tripz = adapter.getTrips(s);
+						for(Trip t : tripz) {
+							ArrayList<StopTime> stopTimes = adapter.getAllStopTimes(s, t);
+							for(StopTime st : stopTimes) {
+								
+							}
 						}
 					}
 				}
@@ -71,8 +107,6 @@ public class Main extends Activity implements LocationListener {
 			}
         	
         }.execute();
-        
-        
     }
 
 	@Override
@@ -82,21 +116,21 @@ public class Main extends Activity implements LocationListener {
 		double wtf = latitude + longitude;
 	}
 
+	/** @see LocationListener#onProviderDisabled(String) */
 	@Override
 	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
 		
 	}
 
+	/** @see LocationListener#onProviderEnabled(String) */
 	@Override
 	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
 		
 	}
 
+	/** @see LocationListener#onStatusChanged(String, int, Bundle) */
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
 		
 	}
 }
