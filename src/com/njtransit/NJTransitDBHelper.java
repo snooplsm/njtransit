eec6433fa9effba9697b6c655754420c80c0468b
doug tangren <d.tangren@gmail.com>
2010-05-16 22:13:10 -0400

    rm unused import

--- src/com/njtransit/NJTransitDBHelper.java
+++ src/com/njtransit/NJTransitDBHelper.java
@@ -3,7 +3,6 @@ package com.njtransit;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.CharBuffer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
