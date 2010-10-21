3b820bfd887398442092928aec6f37865739de99
Ryan Gravener <ryangravener@gmail.com>
2010-10-21 01:48:44 -0400

    major refactor, only thing tha works right now is selecting stations but even that is ugly

--- src/com/njtransit/StationListView.java
+++ src/com/njtransit/StationListView.java
@@ -3,46 +3,58 @@ package com.njtransit;
 import java.util.ArrayList;
 
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.njtransit.domain.Session;
 import com.njtransit.domain.Station;
 import com.njtransit.ui.adapter.StationAdapter;
 
 public class StationListView extends ListView {
-	
+
+	public static interface OnStationListener {
+		void onStationSelected(Station station);
+	}
+
 	public static final int FIRST_STATION_MODE = 0, SECOND_STATION_MODE = 1;
+
 	private Session session = Session.get();
-	
+
 	/** either {@value StationAdapter#ALPHA} or {@value StationAdapter#NEARBY} */
 	private int type = StationAdapter.ALPHA;
-	
+
 	/** either {@value #FIRST_STATION_MODE} of {@value #SECOND_STATION_MODE} */
 	private int mode;
 	
+	private OnStationListener listener;
+	
+	public void setOnStationListener(OnStationListener listener) {
+		this.listener = listener;
+	}
+
 	/** filter the current list down to stations that match name */
 	public StationListView filter(String name) {
 		getStationAdapter().clear();
-		ArrayList<Station> stations = session.getAdapter().getAllStationsLike(name);
-		for(Station s : stations) {
+		ArrayList<Station> stations = session.getAdapter().getAllStationsLike(
+				name);
+		for (Station s : stations) {
 			getStationAdapter().add(s);
 		}
 		return this;
 	}
-	
+
 	public int getMode() {
 		return mode;
 	}
-	
+
 	public StationListView setMode(int mode) {
 		this.mode = mode;
-		if(mode == SECOND_STATION_MODE) {
+		if (mode == SECOND_STATION_MODE) {
 			getStationAdapter().remove(session.getDepartureStation());
 		}
 		return this;
@@ -51,76 +63,85 @@ public class StationListView extends ListView {
 	public Integer getType() {
 		return type;
 	}
-	
+
 	public StationListView setType(int type) {
-		if(this.type == StationAdapter.FAVORITES && type != StationAdapter.FAVORITES) {
+		if (this.type == StationAdapter.FAVORITES
+				&& type != StationAdapter.FAVORITES) {
 			getStationAdapter().clear();
-			for(Station s : session.getStations()) {
+			for (Station s : session.getStations()) {
 				getStationAdapter().add(s);
 			}
 		}
-		if(type == StationAdapter.FAVORITES) {
+		if (type == StationAdapter.FAVORITES) {
 			new AsyncTask<Void, Void, ArrayList<Station>>() {
 				@Override
 				protected ArrayList<Station> doInBackground(Void... params) {
-					ArrayList<Station> stations = session.getAdapter().getMostVisitedStations(session, System.currentTimeMillis());
+					ArrayList<Station> stations = session.getAdapter()
+							.getMostVisitedStations(session,
+									System.currentTimeMillis());
 					return stations;
 				}
 
 				@Override
 				protected void onPostExecute(ArrayList<Station> result) {
 					getStationAdapter().clear();
-					for(Station s : result) {
+					for (Station s : result) {
 						getStationAdapter().add(s);
 					}
-				}				
+				}
 			}.execute();
 		}
 		this.type = type;
 		getStationAdapter().setType(type);
 		invalidateViews();
 		return this;
-	}	
-	
+	}
+
 	public StationListView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);				
+		super(context, attrs, defStyle);
 		// TODO base default type on preference
-		setAdapter(new StationAdapter(context, R.layout.station_row, StationAdapter.ALPHA, session.getStations(), session));
+		setAdapter(new StationAdapter(context, R.layout.station_row,
+				StationAdapter.ALPHA, session.getStations(), session));
 		setType(attrs.getAttributeIntValue(null, "type", StationAdapter.ALPHA));
 		setScrollbarFadingEnabled(true);
 		setFastScrollEnabled(true);
 		setTextFilterEnabled(true);
 		setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-				Station station = getStationAdapter().getItem(position);
-				if(mode == FIRST_STATION_MODE) {	
-					onDepartureSelected(station);
-				} else {
-					onArrivalSelected(station);
-				}	
+			public void onItemClick(AdapterView<?> adapterView, View view,
+					int position, long id) {
+				if(listener!=null) {
+					Station station = getStationAdapter().getItem(position);
+					listener.onStationSelected(station);
+				}
 			}
 		});
 	}
-	
-	private void onDepartureSelected(Station station){
-		Toast.makeText(getContext(), "depart from " + station.getName().toLowerCase(), Toast.LENGTH_SHORT).show();
-		session.setDepartureStation(station);
-		getContext().startActivity(new Intent(getContext(), StationListActivity.class));
-	}
-	
-	private void onArrivalSelected(Station station) {
-		Toast.makeText(getContext(), "arrive at "+station.getName().toLowerCase(), Toast.LENGTH_SHORT).show();
-		session.setArrivalStation(station);
-		getContext().startActivity(new Intent(getContext(), StopActivity.class));
-	}
-	
+
+//	private void onDepartureSelected(Station station) {
+//		Toast.makeText(getContext(),
+//				"depart from " + station.getName().toLowerCase(),
+//				Toast.LENGTH_SHORT).show();
+//		session.setDepartureStation(station);
+//		getContext().startActivity(
+//				new Intent(getContext(), StationListActivity.class));
+//	}
+//
+//	private void onArrivalSelected(Station station) {
+//		Toast.makeText(getContext(),
+//				"arrive at " + station.getName().toLowerCase(),
+//				Toast.LENGTH_SHORT).show();
+//		session.setArrivalStation(station);
+//		getContext()
+//				.startActivity(new Intent(getContext(), StopActivity.class));
+//	}
+
 	/** called by inflater#inflate */
 	public StationListView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
 	
-	private StationAdapter getStationAdapter() {
+	public StationAdapter getStationAdapter() {
 		return (StationAdapter) getAdapter();
 	}
 }
--- src/com/njtransit/ui/adapter/StationAdapter.java
+++ src/com/njtransit/ui/adapter/StationAdapter.java
@@ -8,10 +8,13 @@ import java.util.HashSet;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnTouchListener;
 import android.widget.ArrayAdapter;
 import android.widget.Filterable;
 import android.widget.SectionIndexer;
@@ -23,36 +26,37 @@ import com.njtransit.domain.Station;
 import com.njtransit.utils.Distance;
 import com.njtransit.utils.Locations;
 
-public class StationAdapter extends ArrayAdapter<Station> implements SectionIndexer, Filterable {
-	
+public class StationAdapter extends ArrayAdapter<Station> implements
+		SectionIndexer, Filterable {
+
 	private Session session;
-	
+
 	private int type;
-	
+
 	private HashMap<Integer, Character> posToCharacter;
-	
+
 	private HashMap<Character, Integer> characterToPos;
-	
+
 	private Character[] sections;
-	
+
 	public static final int ALPHA = 0, NEARBY = 1, FAVORITES = 2;
-	
+
 	private static final Comparator<Station> ALPHA_SORT = new Comparator<Station>() {
 		@Override
 		public int compare(Station a, Station b) {
 			return a.getName().compareToIgnoreCase(b.getName());
 		}
 	};
-	
+
 	private static final NearbyComparator NEARBY_SORT = new NearbyComparator();
-	
+
 	private static class NearbyComparator implements Comparator<Station> {
-		
+
 		private Location home;
-		
+
 		public NearbyComparator() {
 		}
-		
+
 		public NearbyComparator setHome(Location l) {
 			this.home = l;
 			return this;
@@ -60,49 +64,66 @@ public class StationAdapter extends ArrayAdapter<Station> implements SectionInde
 
 		@Override
 		public int compare(Station a, Station b) {
-			if(home == null) {
+			if (home == null) {
 				return a.getName().compareToIgnoreCase(b.getName());
 			}
-			Double dist = Distance.greatCircle(home.getLatitude(), home.getLongitude(), a.getLatitude(), a.getLongitude());
-			return dist.compareTo(Distance.greatCircle(home.getLatitude(), home.getLongitude(), b.getLatitude(), b.getLongitude()));
+			Double dist = Distance.greatCircle(home.getLatitude(),
+					home.getLongitude(), a.getLatitude(), a.getLongitude());
+			return dist.compareTo(Distance.greatCircle(home.getLatitude(),
+					home.getLongitude(), b.getLatitude(), b.getLongitude()));
 		}
 	}
-	
+
 	public StationAdapter(Context context, int textViewResourceId, int type,
 			List<Station> items, Session session) {
-		super(context, textViewResourceId, new ArrayList<Station>(items));		
-		this.session = session;		
+		super(context, textViewResourceId, new ArrayList<Station>(items));
+		this.session = session;
 		setType(type);
 	}
-	
+
 	public void setType(int type) {
 		this.type = type;
-		if(type == ALPHA) {
+		if (type == ALPHA) {
 			sort(ALPHA_SORT);
-		} else {		
+		} else {
 			sort(NEARBY_SORT.setHome(session.getLastKnownLocation()));
 		}
 	}
-	
+
 	@Override
 	public View getView(int pos, View convertView, ViewGroup parent) {
-		 View v = getOrInflateView(convertView);
-         Station s = getItem(pos);
-         if (s != null) {
-             TextView name = (TextView) v.findViewById(R.id.station_name);
-             if (name != null) {
-            	 name.setText(s.getName()); 
-             }
-             TextView distance = (TextView) v.findViewById(R.id.station_distance);
-             if(distance != null && session.getLastKnownLocation() != null){
-            	 distance.setText(Locations.relativeDistanceFrom(session.getLastKnownLocation(), session.getPreferences().getUnits()).to(s).inWords());
-             }
-         }
-         return v;
+		View v = getOrInflateView(convertView);
+		v.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				v.setBackgroundColor(Color.parseColor("#F4A83D"));
+				return false;
+			}
+
+		});
+		Station s = getItem(pos);
+		if (s != null) {
+			TextView name = (TextView) v.findViewById(R.id.station_name);
+			if (name != null) {
+				name.setText(s.getName());
+			}
+			TextView distance = (TextView) v
+					.findViewById(R.id.station_distance);
+			if (distance != null && session.getLastKnownLocation() != null) {
+				distance.setText(Locations
+						.relativeDistanceFrom(session.getLastKnownLocation(),
+								session.getPreferences().getUnits()).to(s)
+						.inWords());
+			}
+		}
+		return v;
 	}
-	
+
 	private View getOrInflateView(View v) {
-		return v != null ? v : ((LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.station_row, null);    
+		return v != null ? v : ((LayoutInflater) getContext().getSystemService(
+				Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.station_row,
+				null);
 	}
 
 	@Override
@@ -120,22 +141,23 @@ public class StationAdapter extends ArrayAdapter<Station> implements SectionInde
 
 	@Override
 	public Object[] getSections() {
-		if(type == NEARBY) {
+		if (type == NEARBY) {
 			return null;
 		}
 		calculateSections();
 		return sections;
 	}
-	
+
 	private void calculateSections() {
-		if(sections!=null) return;
-		HashSet<Character> c = new HashSet<Character>();	
+		if (sections != null)
+			return;
+		HashSet<Character> c = new HashSet<Character>();
 		posToCharacter = new HashMap<Integer, Character>();
 		characterToPos = new HashMap<Character, Integer>();
-		for(int i = 0; i < getCount(); i++) {
+		for (int i = 0; i < getCount(); i++) {
 			Station s = getItem(i);
 			char ch = s.getName().charAt(0);
-			if(c.add(ch)) {
+			if (c.add(ch)) {
 				posToCharacter.put(i, ch);
 				characterToPos.put(ch, i);
 			}
