ef2425bff64ba9a1b9ec5856d6cd8d234fa56aa8
softprops <d.tangren@gmail.com>
2011-03-01 01:49:15 -0500

    module for extracting departures from departure vision

--- departurevision/src/main/scala/DepartureVision.scala
+++ departurevision/src/main/scala/DepartureVision.scala
@@ -1,101 +1,80 @@
 package com.gent.departurevision
 
-import scala.xml._
-import java.net.{URLConnection, URL}
-import java.io.{BufferedReader,InputStream,IOException,InputStreamReader}
-import java.util.{HashSet}
+object DepartureVision {
 
-class DepartureVision {
+  /** represents logical unit of data */
+  case class Departure(departs: String, train: String, dest: String,
+                       line: String, track: String, status: String)
 
-	val nj = "http://dv.njtransit.com/mobile/tid-mobile.aspx?sid=";
+  /** xml extractor */
+  case object Extract {
+    import xml.{Node, Elem => El, Text}
+    def unapply(tup: ((Node, Int), (Node, Int))) = tup match {
+      case ((top:Node,_), (bottom:Node,_)) =>
+        val Array(departs, train, dest, line)  = top match {
+          case El(_, "tr", _, _,
+            El(_, "td", _, _,
+              El(_, "a", _, _,
+                El(_, "font", _, _, Text(departsTrain))
+              )
+            ),
+            El(_, "td", _, _,
+              El(_, "font", _, _, Text(dest))
+            ),
+            El(_, "td", _, _,
+              El(_, "font", _, _, Text(line))
+            )
+         ) => departsTrain.split("/").map(_.replace("&nbsp;","").trim()) ++
+              Array(dest.trim(), line.trim())
+       }
+       val (track, status) = bottom match {
+         case El(_,"tr", _, _,
+           El(_, "td", _, _,
+              El(_, "a", _, _,
+                El(_, "font", _, _, Text(track))
+              )
+           ),
+           El(_, "td", _, _,
+             El(_, "font", _, _, Text(status))
+           )
+         ) => (track.replace("&nbsp;","").trim(), status.trim())
+      }
+      Some(Departure(departs, train, dest, line, track, status))
+    }
+  }
 
-	def departures(id:String):Elem = {
-		val url = new java.net.URL(nj + id)
-		toDepartures(convertStream(url.openConnection.getInputStream))
-	}
-	
-	private lazy val tableEnd = "</table>"
-	
-	private def toDepartures(html:StringBuilder):Elem = {
-		val tableStartIndex = html.indexOf("<table");
-		if(tableStartIndex==(-1)) {
-			return null
-		}
-		val tableEndIndex = html.indexOf(tableEnd,tableStartIndex) + tableEnd.length		
-		XML.loadString(html.substring(tableStartIndex,tableEndIndex))
-	}
+	val svc = "http://dv.njtransit.com/mobile/tid-mobile.aspx?sid=%s"
 
-	private val DEPARTS = "DEPARTS"
-	private val TO = "TO"
-	private val TRK = "TRK"
-	private val LINE = "LINE"
-	private val TRAIN = "TRAIN"	
-	private val STATUS = "STATUS"	
+  /** scape all departures for a station id and return a handler fn */
+	def departures[T](id: String)(f: (Seq[Departure] => T)): T =
+		table(id) { asXml(_)(scrape(_)(f)) }
 
-	def filterDepartures(feed:Elem, feedId:String):HashSet[Departure] = {
-	   var foundHeader = false
-	   var departPos = -1
-	   var toPos = -1
-	   var trkPos = -1
-	   var linePos = -1
-	   var trainPos = -1
-	   var statusPos = -1
-	   var departures = new HashSet[Departure]
-	   feed \\ "tr" foreach { (tr:Node) =>
-	     if (!isHeader(tr)) {
-			if(!foundHeader) {
-				departPos = tr \\ "td" findIndexOf {_.text == DEPARTS}
- 				toPos = tr \\ "td" findIndexOf {_.text == TO}
-				trkPos = tr \\ "td" findIndexOf {_.text == TRK}
-				linePos = tr \\ "td" findIndexOf {_.text == LINE}
-				trainPos =  tr \\ "td" findIndexOf {_.text == TRAIN}
-				statusPos = tr \\ "td" findIndexOf {_.text == STATUS}
-				foundHeader = true
-			} else {
-				var departure = new Departure(null,null,null,null,null,null)	
-								
-				(tr \\ "td").view.zipWithIndex foreach { e => 
-					e._2 match {
-						case departPos => println("e")
-					}
-/*					e._2 match {
-						case departPos => departure.departs = e._1.text
-						case linePos => departure.line = e._1.text
-						case trainPos => departure.train = e._1.text
-						case statusPos => departure.status = e._1.text
-						case toPos => departure.to = e._1.text
-						case trkPos => departure.track = e._1.text
-						case _ => println("no dice")
-					}
-*/				
- 				}
-		 		//departures.add(departure)
-			}
-	     }
-	   }
-		return departures
-	 }
-	
-	def isHeader(p:Node):Boolean = {
-		 (p \\ "@colspan" == (5)) 
-	 }
-	
-	private def convertStream(is: InputStream):StringBuilder = {
-	    val reader = new BufferedReader(new InputStreamReader(is));
-	    val sb = new StringBuilder();
+  /** normalize and return iterable seq of strings representing table */
+  private def table[T](id: String)(f: Iterable[String] => T): T =
+    f("<table>" :: (
+      ("</table>" :: (
+       io.Source.fromURL(svc format id).
+       getLines().
+       takeWhile(!_.startsWith("</table>")).
+       drop(2).
+       filter(!_.startsWith("<hr>")).
+       toList.
+       reverse)
+     ).
+     reverse))
 
-	    var line : String = null
-	    try {
-	      while ({line = reader.readLine();  line!= null}) {
-	        sb.append(line + "\n")
-	      }
-	    } finally {
-	      	try {
-	        	is.close()
-	      	} catch {
-	        	case e: IOException => e.printStackTrace()
-	      	}
-	    }
-		return sb
-	  }
+  /** if exception occurs return empty list */
+  private def asXml[T](it: Iterable[String])(f: xml.NodeSeq=>T): T =
+    f(try{
+       xml.XML.loadString(it.mkString("").replace("&", "&amp;"))
+     } catch { case _ => Nil })
+
+  /** logical units of data come in two rows */
+  private def scrape[T](data: xml.NodeSeq)(f: Seq[Departure]=> T): T =
+    f((((data \\ "tr").zipWithIndex.partition(_._2 % 2 == 0))
+      match { case (l1, l2) => l1 zip l2  }) map { _  match {
+        case Extract(departure) => departure
+      }
+   })
+
 }
