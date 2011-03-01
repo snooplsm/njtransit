37183095f1f486df85b2e607da4ba176d303f806
softprops <d.tangren@gmail.com>
2011-03-01 02:06:45 -0500

    prove it works

---
+++ departurevision/src/test/scala/DepartureVisionSpec.scala
@@ -0,0 +1,19 @@
+package com.gent.departurevision
+
+import org.specs._
+
+object DepartureVisionSpec extends Specification {
+  "DepartureVision" should {
+    "scape data for valid id" in {
+      val departures = DepartureVision.departures("NY") { d =>
+       d.foreach(println)
+       d
+      }
+      departures.size must be > 0
+    }
+    "fail gracefully with invalid id" in {
+      val departures = DepartureVision.departures("bogus")(d=>d)
+      departures must beEmpty
+    }
+  }
+}
